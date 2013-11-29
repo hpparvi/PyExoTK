7e988b9310eb3091d33a06e337d9410051b94b62
Hannu Parviainen <hpparvi@gmail.com>
2013-11-29 17:50:56 +0000

    Introduced an eclipse module, split the constants properly into constants and utils/orbits

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,15 +1,69 @@
 import numpy as np
+from scipy.constants import c, k, h
 
-def fold(x, period, origo=0.0, shift=0.0, normalize=True,  clip_range=None):
+def fold(time, period, origo=0.0, shift=0.0, normalize=True,  clip_range=None):
+    """Folds the given data over a given period.
+
+    Parameters
+    ----------
+    
+      time        
+      period      
+      origo       
+      shift       
+      normalize   
+      clip_range  
+
+    Returns
+    -------
+
+      phase       
     """
-    Folds the given data over a given period.
-    """
-    xf = ((x - origo)/period + shift) % 1.
+    tf = ((time - origo)/period + shift) % 1.
 
     if not normalize:
-        xf *= period
+        tf *= period
         
     if clip_range is not None:
-        mask = np.logical_and(clip_range[0]<xf, xf<clip_range[1])
-        xf = xf[mask], mask
-    return xf
+        mask = np.logical_and(clip_range[0]<tf, tf<clip_range[1])
+        tf = tf[mask], mask
+    return tf
+
+
+def planck(T, wl):
+    """Radiance as a function or black-body temperature and wavelength.
+
+    Parameters
+    ----------
+
+      T   : Temperature  [K]
+      wl  : Wavelength   [m]
+
+    Returns
+    -------
+
+      B   : Radiance
+    """
+    return 2*h*c**2/wl**5 / (np.exp(h*c/(wl*k*T))-1)
+
+
+def contamination_bb(c1, T, wl1, wl2):
+    """Contamination from a third object radiating as a black-body given a contamination estimate in a reference wavelength. 
+
+    Parameters
+    ----------
+
+      c1   : Contamination in the reference wavelength [-]
+      T    : Temperature                               [K]
+      wl1  : Reference wavelength                      [m]
+      wl2  : Target wavelength                         [m]
+
+    Returns
+    -------
+
+      c2  : Contamination in the given wavelength      [-]
+    """
+    B1   = planck(T, wl1)
+    B2   = planck(T, wl2)
+
+    return  c1*(B2/B1)
---
+++ src/utils/orbits.py
@@ -0,0 +1,57 @@
+def as_from_rhop(rho, period):
+    """Scaled semi-major axis from the stellar density and planet's orbital period.
+
+    Parameters
+    ----------
+
+      rho    : stellar density [g/cm^3]
+      period : orbital period  [d]
+
+    Returns
+    -------
+
+      as : scaled semi-major axis [R_star]
+    """
+    return (G/(3*pi))**(1/3)*((period*86400.)**2 * 1e3*rho)**(1/3)
+
+
+def a_from_rhoprs(rho, period, rstar):
+    """Semi-major axis from the stellar density, stellar radius, and planet's orbital period.
+
+    Parameters
+    ----------
+
+      rho    : stellar density [g/cm^3]
+      period : orbital period  [d]
+      rstar  : stellar radius  [R_Sun]
+
+    Returns
+    -------
+
+      a : semi-major axis [AU]
+    """
+    return as_from_rhop(rho,period)*rstar*rsun/au
+
+
+def af_transit(e,w):
+    """Calculates the -- factor during the transit"""
+    return (1.0-e**2)/(1.0 + e*np.sin(w))
+
+
+def i_from_baew(b,a,e,w):
+    """Orbital inclination from the impact parameter, scaled semi-major axis, eccentricity and argument of periastron
+
+    Parameters
+    ----------
+
+      b  : impact parameter       [-]
+      a  : scaled semi-major axis [R_Star]
+      e  : eccentricity           [-]
+      w  : argument of periastron [rad]
+
+    Returns
+    -------
+
+      i  : inclination            [rad]
+    """
+    return np.arccos(b / (a*af_transit(e, w)))
